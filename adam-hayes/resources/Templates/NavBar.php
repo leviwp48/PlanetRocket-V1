62af6e4e04d2ac2c396f0e57fad547c0ef45ac5f
Eric Mott <32501191+emott10@users.noreply.github.com>
2018-09-14 13:54:56 -0700

    Update NavBar.php

--- adam-hayes/resources/Templates/NavBar.php
+++ adam-hayes/resources/Templates/NavBar.php
@@ -44,10 +44,10 @@ private $_forHomePage = false;
 	                    <li class="box top-nav-item">
 	                       <a class="row" href="<?php echo APP_BASE; ?>/projects/all" style="padding:8px; padding-left:20px; padding-right:20px;font-family: 'Open Sans', sans-serif;">Projects</a>
 	                    </li>
-											<!--<li class="box top-nav-item">
-												 <a class="row" href="<?php echo APP_BASE; ?>/training" style="padding:8px; padding-left:20px; padding-right:20px;font-family: 'Open Sans', sans-serif;">Training</a>
-											</li>
-											<li class="box top-nav-item">
+			    <li class="box top-nav-item">
+				<a class="row" href="<?php echo APP_BASE; ?>/training" style="padding:8px; padding-left:20px; padding-right:20px;font-family: 'Open Sans', sans-serif;">Training</a>
+			    </li>
+			    <!--<li class="box top-nav-item">
 	                       <a class="row" href="/calendar" style="padding:8px; padding-left:20px; padding-right:20px;">Calendar</a>
 	                    </li>-->
 						
